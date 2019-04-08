e041c6970e866c2c47a48690a7b4d5321dbb4173
Afonso Gloeden <afgloeden@gmail.com>
2019-04-08 19:24:57 -0300

    TAB characters converted to 4 spaces (PSR-2)

--- Database/Query/SybaseGrammar.php
+++ Database/Query/SybaseGrammar.php
@@ -5,16 +5,16 @@ use Illuminate\Database\Query\Grammars\Grammar;
 
 class SybaseGrammar extends Grammar {
 
-	/**
-	 * All of the available clause operators.
-	 *
-	 * @var array
-	 */
+    /**
+     * All of the available clause operators.
+     *
+     * @var array
+     */
         protected $operators = array(
-		'=', '<', '>', '<=', '>=', '!<', '!>', '<>', '!=',
-		'like', 'not like', 'between', 'ilike',
-		'&', '&=', '|', '|=', '^', '^=',
-	);
+        '=', '<', '>', '<=', '>=', '!<', '!>', '<>', '!=',
+        'like', 'not like', 'between', 'ilike',
+        '&', '&=', '|', '|=', '^', '^=',
+    );
         
         protected $Builder;
         public function getBuilder(){
@@ -23,120 +23,120 @@ class SybaseGrammar extends Grammar {
         
 
         /**
-	 * Compile a select query into SQL.
-	 *
-	 * @param  \Illuminate\Database\Query\Builder
-	 * @return string
-	 */
+     * Compile a select query into SQL.
+     *
+     * @param  \Illuminate\Database\Query\Builder
+     * @return string
+     */
         
-	public function compileSelect(Builder $query)
-	{
+    public function compileSelect(Builder $query)
+    {
                 $this->Builder = $query;
-		$components = $this->compileComponents($query);
+        $components = $this->compileComponents($query);
 
-		return $this->concatenate($components);
-	}
-	/**
-	 * Compile the "select *" portion of the query.
-	 *
-	 * @param  \Illuminate\Database\Query\Builder  $query
-	 * @param  array  $columns
-	 * @return string
-	 */
-	protected function compileColumns(Builder $query, $columns)
-	{
-		if ( ! is_null($query->aggregate)) return;
+        return $this->concatenate($components);
+    }
+    /**
+     * Compile the "select *" portion of the query.
+     *
+     * @param  \Illuminate\Database\Query\Builder  $query
+     * @param  array  $columns
+     * @return string
+     */
+    protected function compileColumns(Builder $query, $columns)
+    {
+        if ( ! is_null($query->aggregate)) return;
 
-		$select = $query->distinct ? 'select distinct ' : 'select ';
+        $select = $query->distinct ? 'select distinct ' : 'select ';
 
-		// If there is a limit on the query, but not an offset, we will add the top
-		// clause to the query, which serves as a "limit" type clause within the
-		// SQL Server system similar to the limit keywords available in MySQL.
-		if ($query->limit > 0 && $query->offset <= 0)
-		{
-			$select .= 'top '.$query->limit.' ';
-		}
+        // If there is a limit on the query, but not an offset, we will add the top
+        // clause to the query, which serves as a "limit" type clause within the
+        // SQL Server system similar to the limit keywords available in MySQL.
+        if ($query->limit > 0 && $query->offset <= 0)
+        {
+            $select .= 'top '.$query->limit.' ';
+        }
 
-		return $select.$this->columnize($columns);
-	}
+        return $select.$this->columnize($columns);
+    }
 
-	/**
-	 * Compile the "from" portion of the query.
-	 *
-	 * @param  \Illuminate\Database\Query\Builder  $query
-	 * @param  string  $table
-	 * @return string
-	 */
-	protected function compileFrom(Builder $query, $table)
-	{
-		$from = parent::compileFrom($query, $table);
+    /**
+     * Compile the "from" portion of the query.
+     *
+     * @param  \Illuminate\Database\Query\Builder  $query
+     * @param  string  $table
+     * @return string
+     */
+    protected function compileFrom(Builder $query, $table)
+    {
+        $from = parent::compileFrom($query, $table);
 
-		if (is_string($query->lock)) return $from.' '.$query->lock;
+        if (is_string($query->lock)) return $from.' '.$query->lock;
 
-		if ( ! is_null($query->lock))
-		{
-			return $from.' with(rowlock,'.($query->lock ? 'updlock,' : '').'holdlock)';
-		}
+        if ( ! is_null($query->lock))
+        {
+            return $from.' with(rowlock,'.($query->lock ? 'updlock,' : '').'holdlock)';
+        }
 
-		return $from;
-	}
-	
-	/**
-	 * Compile the "limit" portions of the query.
-	 *
-	 * @param  \Illuminate\Database\Query\Builder  $query
-	 * @param  int  $limit
-	 * @return string
-	 */
-	protected function compileLimit(Builder $query, $limit)
-	{
-		return '';
-	}
+        return $from;
+    }
+    
+    /**
+     * Compile the "limit" portions of the query.
+     *
+     * @param  \Illuminate\Database\Query\Builder  $query
+     * @param  int  $limit
+     * @return string
+     */
+    protected function compileLimit(Builder $query, $limit)
+    {
+        return '';
+    }
 
-	/**
-	 * Compile the "offset" portions of the query.
-	 *
-	 * @param  \Illuminate\Database\Query\Builder  $query
-	 * @param  int  $offset
-	 * @return string
-	 */
-	protected function compileOffset(Builder $query, $offset)
-	{
-		return '';
-	}
+    /**
+     * Compile the "offset" portions of the query.
+     *
+     * @param  \Illuminate\Database\Query\Builder  $query
+     * @param  int  $offset
+     * @return string
+     */
+    protected function compileOffset(Builder $query, $offset)
+    {
+        return '';
+    }
 
-	/**
-	 * Compile a truncate table statement into SQL.
-	 *
-	 * @param  \Illuminate\Database\Query\Builder  $query
-	 * @return array
-	 */
-	public function compileTruncate(Builder $query)
-	{
-		return array('truncate table '.$this->wrapTable($query->from) => array());
-	}
+    /**
+     * Compile a truncate table statement into SQL.
+     *
+     * @param  \Illuminate\Database\Query\Builder  $query
+     * @return array
+     */
+    public function compileTruncate(Builder $query)
+    {
+        return array('truncate table '.$this->wrapTable($query->from) => array());
+    }
 
-	/**
-	 * Get the format for database stored dates.
-	 *
-	 * @return string
-	 */
-	public function getDateFormat()
-	{
-		return 'Y-m-d H:i:s.000';
-	}
+    /**
+     * Get the format for database stored dates.
+     *
+     * @return string
+     */
+    public function getDateFormat()
+    {
+        return 'Y-m-d H:i:s.000';
+    }
 
-	/**
-	 * Wrap a single string in keyword identifiers.
-	 *
-	 * @param  string  $value
-	 * @return string
-	 */
-	protected function wrapValue($value)
-	{
-		if ($value === '*') return $value;
+    /**
+     * Wrap a single string in keyword identifiers.
+     *
+     * @param  string  $value
+     * @return string
+     */
+    protected function wrapValue($value)
+    {
+        if ($value === '*') return $value;
 
-		return '['.str_replace(']', ']]', $value).']';
-	}
+        return '['.str_replace(']', ']]', $value).']';
+    }
 
 }
